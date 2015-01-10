4091107749ad23a0dc494813db8b5ecc29b01036
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-10 22:34:30 +0100

    :-1:  Well, let's just say I am having a bad code day

--- src/js/app.js
+++ src/js/app.js
@@ -7,15 +7,43 @@
    * Main controller
    */
   .controller('JobbController', function($scope) {
-    var nightMode;
-    nightMode = false;
+
+    var nightMode = false;
+
     if (localStorage['isNightMode'] === '' || (localStorage['isNightMode'] == null)) {
+
       $scope.isNightMode = nightMode;
+
     } else {
+
       $scope.isNightMode = JSON.parse(localStorage['isNightMode']);
+
     }
+
     $scope.$watch('isNightMode', (function() {
+
       localStorage['isNightMode'] = JSON.stringify($scope.isNightMode);
+
     }), true);
+
+    $scope.getAge = function(year, month, day) {
+      var now = new Date();
+      var age = now.getFullYear() - year;
+      var mdif = now.getMonth() - month + 1; //0=jan
+
+      if(mdif < 0) {
+        --age;
+      }
+      else if(mdif === 0) {
+        var ddif = now.getDate() - day;
+
+        if(ddif < 0) {
+          --age;
+        }
+      }
+
+      return age;
+    }
+
   });
 })(window.angular);
